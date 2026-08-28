cb1f33ba1f807d0915c98edbc03c3e2148a433b2
kg_dev <kiansalaysay846@gmail.com>
2026-08-28 14:31:35 +0800

    Revert "new changes"
    This reverts commit 72864f9528c4cb320940ae3d146ecd65e697b946.

--- src/features/tickets/components/TicketModal.tsx
+++ src/features/tickets/components/TicketModal.tsx
@@ -1,6 +1,5 @@
 import Modal from "@/components/ui/Modal";
 import styles from './CreateTicketModal.module.css';
-import logo from '../../../assets/icons/Marikina_City_Seal.svg (1).webp';
 
 interface TicketModalProps {
     isOpen: boolean,
@@ -9,5 +8,21 @@ interface TicketModalProps {
 }
 
 export function TicketModal({ isOpen, ticketId, onClose }: TicketModalProps) {
-    
+    return (
+        <Modal
+            open={isOpen}
+            title="TicketRecord"
+            subtitle="Here is the record of the collected violation ticket. You can review the details below."
+            onClose={onClose}
+            size="lg"
+        >
+            <div className={styles.layout}>
+                <div className={styles.ticketPaper}>
+                    <div className={styles.ticketHeader}>
+                        <h4>City of Marikina</h4>
+                    </div>
+                </div>
+            </div>
+        </Modal>
+    );
 }
